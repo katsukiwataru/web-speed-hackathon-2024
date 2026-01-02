c3deb4bab6bf7be82abab8f91c0ec0338e980247
katsukiwataru <katsukiwataru0109@gmail.com>
2026-01-02 21:39:40 +0900

    chore: useImage のリファクタ

--- workspaces/app/src/foundation/hooks/useImage.ts
+++ workspaces/app/src/foundation/hooks/useImage.ts
@@ -1,17 +1,10 @@
-import { useSyncExternalStore } from 'react';
-
 import { getImageUrl } from '../../lib/image/getImageUrl';
 
-const getDpr = () => (typeof window !== 'undefined' ? window.devicePixelRatio : 1);
-const subscribe = () => () => {};
-
 export const useImage = ({ height, imageId, width }: { height: number; imageId: string; width: number }) => {
-  const dpr = useSyncExternalStore(subscribe, getDpr, () => 1);
-
   return getImageUrl({
     format: 'webp',
-    height: Math.round(height * dpr),
+    height,
     imageId,
-    width: Math.round(width * dpr),
+    width,
   });
 };
